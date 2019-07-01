3c3ec118b51c7b1ee28405ff7b722feb684f22a6
Stefan Stojanovic <stefan.stojanovic2202@gmail.com>
2019-06-30 21:00:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+// 1
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+// 2
+let random = Math.ceil(Math.random());
+
+// 3 
+let mod = 29 % 5
+
+// 4
+let max = Math.max(1, 20)
+
